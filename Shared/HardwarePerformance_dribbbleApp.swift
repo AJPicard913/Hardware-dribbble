7d27c82c34958c295a46c7fda8ef5bd49fa56d1b
ajpicard913 <Contact@saucedesigns.io>
2020-08-21 08:56:26 -0400

    Initial Commit

---
+++ Shared/HardwarePerformance_dribbbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardwarePerformance_dribbbleApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 8/21/20.
+//
+
+import SwiftUI
+
+@main
+struct HardwarePerformance_dribbbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
